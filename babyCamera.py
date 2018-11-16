34fc2812cc374e617128dce526616a29345ade7c
Nico <ncai@duenorthsystems.org>
2018-11-15 21:53:18 -0600

    modify structure of project

---
+++ babyCamera.py
@@ -0,0 +1,31 @@
+import firebase_admin
+from firebase_admin import credentials
+from firebase_admin import firestore
+
+
+# Use a service account
+cred = credentials.Certificate('hometemomonitoring-9ec2bf60f58a.json')
+firebase_admin.initialize_app(cred)
+
+db = firestore.client()
+
+doc_ref = db.collection(u'users').document(u'alovelace')
+doc_ref.set({
+    u'first': u'Ada',
+    u'last': u'Lovelace',
+    u'born': 1813
+})
+
+doc_ref = db.collection(u'users').document(u'aturing')
+doc_ref.set({
+    u'first': u'Alan',
+    u'middle': u'Mathison',
+    u'last': u'Turing',
+    u'born': 1912
+})
+
+users_ref = db.collection(u'users')
+docs = users_ref.get()
+
+for doc in docs:
+    print(u'{} => {}'.format(doc.id, doc.to_dict()))
